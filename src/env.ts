152cfbcc019213a51e82692f0ad82fe8ff19f863
joas-vieira <joas.vieira.dev@gmail.com>
2025-03-11 07:34:31 -0300

    feat: config database for development

--- src/env.ts
+++ src/env.ts
@@ -3,7 +3,9 @@ import { z } from 'zod';
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'production', 'test']),
-  PORT: z.coerce.number()
+  PORT: z.coerce.number(),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
+  DATABASE_URL: z.string()
 });
 
 export const env = envSchema.parse(process.env);
